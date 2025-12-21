66a90d43cc05e797a9b9d87792b83bded04d859c
AnasTorky <143271015+AnasTorky@users.noreply.github.com>
2025-12-21 12:04:15 +0200

    MappersImpl & ControllersCreate & ServicesUpdate (#10)
    * mappers,sequences,base-entity
    
    * MappersImpl & ControllersCreate & ServicesUpdate

--- Backend/ZakahCalculator/src/main/java/ntg/project/ZakahCalculator/mapper/RoleMapper.java
+++ Backend/ZakahCalculator/src/main/java/ntg/project/ZakahCalculator/mapper/RoleMapper.java
@@ -1,4 +1,26 @@
 package ntg.project.ZakahCalculator.mapper;
 
+<<<<<<< HEAD
+import ntg.project.ZakahCalculator.entity.Role;
+import ntg.project.ZakahCalculator.entity.util.UserType;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface RoleMapper {
+
+    default Role fromUserType(UserType userType) {
+        if (userType == null) return null;
+
+        return Role.builder()
+                .name(userType)
+                .build();
+    }
+
+    default UserType toUserType(Role role) {
+        if (role == null) return null;
+        return role.getName();
+    }
+=======
+public interface RoleMapper {
+>>>>>>> 014a1fd10945a19fe5b84da52a2dd6ccb772e5ba
 }
--- Backend/ZakahCalculator/src/main/java/ntg/project/ZakahCalculator/service/impl/RoleServiceImpl.java
+++ Backend/ZakahCalculator/src/main/java/ntg/project/ZakahCalculator/service/impl/RoleServiceImpl.java
@@ -1,15 +1,13 @@
 package ntg.project.ZakahCalculator.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import ntg.project.ZakahCalculator.entity.Role;
 import ntg.project.ZakahCalculator.entity.util.UserType;
 import ntg.project.ZakahCalculator.exception.BusinessException;
 import ntg.project.ZakahCalculator.exception.ErrorCode;
 import ntg.project.ZakahCalculator.repository.RoleRepository;
 import ntg.project.ZakahCalculator.service.RoleService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +18,10 @@ public class RoleServiceImpl implements RoleService {
     @Override
     public Role findByName(UserType name) {
         return roleRepository.findByName(name)
+<<<<<<< HEAD
+                .orElseThrow(() -> new BusinessException(ErrorCode.ROLE_NOT_FOUND));
+=======
                 .orElseThrow(()-> new BusinessException(ErrorCode.ROLE_NOT_FOUND));
+>>>>>>> 014a1fd10945a19fe5b84da52a2dd6ccb772e5ba
     }
 }
